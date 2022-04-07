10a4383880e4308ae4686c35f691443b87356806
Dillon Schultz <23678183+dillonschultz93@users.noreply.github.com>
2022-04-07 12:30:27 -0700

    Use the tokens package as an entry point for the Figma Tokens plugin

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,26 +1,11 @@
 import fs from 'fs';
 import path from 'path';
-import { writeTokensToJSON } from './utilities/writeTokensToJSON';
-import { generateSizing } from './sizing';
-import { generateColorScales } from './colors';
 
-const configFile = fs.readFileSync(path.join(__dirname, '../config.json'), 'utf8');
+const configFile = fs.readFileSync(path.join(__dirname, '../tokens.json'), 'utf8');
 const parsedConfig = JSON.parse(configFile);
 
-const { sizing, typography, colors } = parsedConfig;
-
 const QuietoDesignTokens = {
-  spaces: generateSizing(sizing),
-  typography: {
-    ...typography,
-  },
-  colors: {
-    ...generateColorScales(colors),
-    white: '#ffffff',
-    black: '#000000',
-  },
+  ...parsedConfig,
 };
-
-writeTokensToJSON(QuietoDesignTokens);
 
 export default QuietoDesignTokens;
--- packages/tokens/src/utilities/scale.ts
+++
@@ -1,7 +0,0 @@
-export const scaleA = (base: number, ratio: number, currentIndex: number) => {
-  return base + ratio * (currentIndex / 100 - 1);
-};
-
-export const scaleB = (base: number, ratio: number, currentIndex: number) => {
-  return base + 30 + ratio * 8 * (currentIndex / 100 - 1);
-};
