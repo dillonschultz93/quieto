54934d4ce5e78012514c8579f9013d10d490a878
Dillon Schultz <23678183+dillonschultz93@users.noreply.github.com>
2022-04-07 20:50:45 -0700

    Transform tokens on build

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
-const configFile = fs.readFileSync(path.join(__dirname, '../tokens.json'), 'utf8');
+const configFile = fs.readFileSync(path.join(__dirname, '../parsed-tokens.json'), 'utf8');
 const parsedConfig = JSON.parse(configFile);
 
 const QuietoDesignTokens = {
   ...parsedConfig,
 };
+
+console.log(JSON.stringify(QuietoDesignTokens, null, 2));
 
 export default QuietoDesignTokens;
