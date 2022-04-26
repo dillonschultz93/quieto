ff511e41a0521b1b968f5620cab5ed896d5d8339
Dillon Schultz <dillonschultz93@gmail.com>
2022-04-26 16:36:45 -0700

    Name export instead of a default export

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -4,8 +4,6 @@ import path from 'path';
 const tokenFile = fs.readFileSync(path.join(__dirname, 'parsed-tokens.json'), 'utf8');
 const parsedTokens = JSON.parse(tokenFile);
 
-const QuietoDesignTokens = {
+export const QuietoDesignTokens = {
   ...parsedTokens,
 };
-
-export default QuietoDesignTokens;
