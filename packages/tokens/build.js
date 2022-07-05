7652b1c5a3f023c4540b8d256a837d5f6271a46a
Dillon Schultz <23678183+dillonschultz93@users.noreply.github.com>
2022-07-05 16:21:03 -0700

    Import transformers and reference in platforms

--- packages/tokens/build.js
+++ packages/tokens/build.js
@@ -1,104 +1,153 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
-const path = require('path');
-const _ = require('lodash');
-const { transform } = require('@divriots/style-dictionary-to-figma');
-const stripTokenset = require('./util/strip-tokenset');
+// const path = require('path');
+// const _ = require('lodash');
+const StyleDictionary = require('style-dictionary');
+// const { transform } = require('@divriots/style-dictionary-to-figma');
 
 // Set up style dictionary ====================================================
-const StyleDictionary = require('style-dictionary').extend({
-  "source": ["src/**/*.tokens.json"],
-  "format": {
-    figmaTokensPluginJson: ({ dictionary }) => {
-      const transformedTokens = transform(dictionary.tokens);
+const modes = ['light', 'dark'];
 
-      return JSON.stringify(transformedTokens, null, 2);
-    }
+// Build light mode tokens
+StyleDictionary.extend({
+  transform: {
+    // Typography transforms
+    // CSS
+    'fontSize/css': require('./transformers/typography/font-size-css'),
+    'tracking/css': require('./transformers/typography/tracking-css'),
+    'paragraphSpacing/css': require('./transformers/typography/paragraph-spacing-css'),
+    // Figma
+
+    // Dimension transforms
+    // CSS
+    'sizing/css': require('./transformers/dimension/size-css'),
+    'spacing/css': require('./transformers/dimension/space-css'),
+    'corners/css': require('./transformers/dimension/corner-css'),
+    'borderWidth/css': require('./transformers/dimension/border-width-css'),
   },
-  "platforms": {
-    "js": {
-      "transformGroup": "js",
-      "buildPath": "lib/js/",
-      "files": [
+  source: [`src/**/!(*.${modes.join(`|*.`)}).tokens.json`],
+  platforms: {
+    css: {
+      transforms: [
+        'attribute/cti',
+        'name/cti/kebab',
+        'time/seconds',
+        'content/icon',
+        'fontSize/css',
+        'tracking/css',
+        'paragraphSpacing/css',
+        'sizing/css',
+        'spacing/css',
+        'corners/css',
+        'borderWidth/css',
+        'color/css',
+      ],
+      buildPath: 'lib/css/',
+      files: [
         {
-          "destination": "index.js",
-          "format": "javascript/es6"
-        }
-      ]
-    },
-    "ts": {
-      "transformGroup": "js",
-      "buildPath": "lib/js/",
-      "files": [
-        {
-          "destination": "index.d.ts",
-          "format": "typings/es6"
-        }
-      ]
-    },
-    "css": {
-      "transformGroup": "css",
-      "buildPath": "lib/css/",
-      "files": [
-        {
-          "destination": "_tokens.css",
-          "format": "css/variables",
+          destination: '_variables.css',
+          format: 'css/variables',
           options: {
-            outputReferences: true
-          }
-        }
-      ]
+            outputReferences: true,
+          },
+        },
+      ],
     },
-    "scss": {
-      "transformGroup": "css",
-      "buildPath": "lib/scss/",
-      "files": [
+    json: {
+      transforms: [
+        'attribute/cti',
+        'name/cti/pascal',
+        'fontSize/css',
+        'tracking/css',
+        'paragraphSpacing/css',
+        'sizing/css',
+        'spacing/css',
+        'corners/css',
+        'borderWidth/css',
+        'color/hex',
+      ],
+      buildPath: 'src/',
+      files: [
         {
-          "destination": "_tokens.scss",
-          "format": "scss/variables",
+          destination: '../tmp/transformed-tokens.json',
+          format: 'json/nested',
+        },
+      ],
+    },
+  },
+}).buildAllPlatforms();
+
+// // Build dark mode tokens
+StyleDictionary.extend({
+  include: [`src/**/!(*.${modes.join(`|*.`)}).tokens.json`],
+  source: ['src/**/*.dark.tokens.json'],
+  platforms: {
+    dark_css: {
+      transformGroup: 'css',
+      buildPath: 'lib/css/',
+      files: [
+        {
+          destination: '_dark-variables.css',
+          format: 'css/variables',
+          filter: token => token.filePath.indexOf('.dark.') > -1,
           options: {
-            outputReferences: true
-          }
-        }
-      ]
+            outputReferences: true,
+          },
+        },
+      ],
     },
-    "json": {
-      "transformGroup": "js",
-      "buildPath": "src/",
-      "files": [
+    dark_json: {
+      transformGroup: 'js',
+      buildPath: 'src/',
+      files: [
         {
-          "destination": "../tmp/transformed-tokens.json",
-          "format": "json/nested"
-        }
-      ]
+          destination: '../tmp/transformed-dark-tokens.json',
+          format: 'json/nested',
+          filter: token => token.filePath.indexOf('.dark.') > -1,
+        },
+      ],
     },
-    "figma": {
-      "transformGroup": "js",
-      "buildPath": "figma/",
-      "files": [
-        {
-          "destination": "figma-tokens.json",
-          "format": "figmaTokensPluginJson"
+  },
+}).buildAllPlatforms();
+
+// Create a custom formatter for style dictionary
+// const templateFile = fs.readFileSync(path.join(__dirname, 'templates/es6.template'), 'utf-8');
+// const typingES6Template = _.template(templateFile);
+
+// StyleDictionary.registerFormat({
+//   name: 'typings/es6',
+//   formatter: typingES6Template
+// });
+// =============================================================================
+
+const tokens = require('./tmp/transformed-tokens.json');
+const darkTokens = require('./tmp/transformed-dark-tokens.json');
+
+let transformedTokens = { ...tokens };
+
+// Compare the two tokensets
+Object.entries(darkTokens).forEach(([category, categoryValue]) => {
+  if (category in tokens) {
+    // Check if the category is either color of component
+    if (category === 'color' || category === 'component') {
+      transformedTokens = {
+        ...transformedTokens,
+        [category]: {},
+      };
+      // Loop once more through to get the type of the token
+      Object.entries(categoryValue).forEach(([type, typeValue]) => {
+        if (type in tokens[category]) {
+          transformedTokens[category] = {
+            ...transformedTokens[category],
+            [type]: {
+              dark: typeValue,
+              light: tokens[category][type],
+            },
+          };
         }
-      ]
+      });
     }
   }
 });
 
-// Create a custom formatter for style dictionary
-const templateFile = fs.readFileSync(path.join(__dirname, 'templates/es6.template'), 'utf-8');
-const typingES6Template = _.template(templateFile);
-
-StyleDictionary.registerFormat({
-  name: 'typings/es6',
-  formatter: typingES6Template
-});
-// =============================================================================
-
-StyleDictionary.buildAllPlatforms();
-
-// Strip the `tokenset` property from the transformed tokens
-const transformedTokens = require('./tmp/transformed-tokens.json');
-const strippedTokens = stripTokenset(transformedTokens);
-
-fs.writeFileSync('./src/tokens.json', JSON.stringify(strippedTokens, null, 2));
+fs.writeFileSync('./src/tokens.json', JSON.stringify(transformedTokens, null, 2));
